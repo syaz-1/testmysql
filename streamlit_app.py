2f3d0e0ff6cf713d7d0c87f1fca7e9c4d83b9fcf
syaz-1 <83575979+syaz-1@users.noreply.github.com>
2024-07-24 03:12:01 +0000

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,17 +1,21 @@
 import streamlit as st
 import pandas as pd
 import requests
+from sqlalchemy.sql import text
+
+conn=st.connection('mysql',type='sql')
+
 # import mysql.connector
-from oci.config import from_file
-from oci.signer import Signer
-config = from_file()
-auth = Signer(
-tenancy=config['tenancy'],
-user=config['user'],
-fingerprint=config['fingerprint'],
-private_key_file_location=config['key_file'],
-pass_phrase=config['pass_phrase']
-)
+# from oci.config import from_file
+# from oci.signer import Signer
+# config = from_file()
+# auth = Signer(
+# tenancy=config['tenancy'],
+# user=config['user'],
+# fingerprint=config['fingerprint'],
+# private_key_file_location=config['key_file'],
+# pass_phrase=config['pass_phrase']
+# )
 
 endpoint='https://sql.dbtools.us-chicago-1.oci.oraclecloud.com/20201005/ords/ocid1.databasetoolsconnection.oc1.us-chicago-1.amaaaaaaxc7u3kianrg7kpoftbqvafpy5eghxszddftbnwbao2ze5x6hemiq/_/sql'
 headers={'Content-Type': 'application/sql'}
@@ -48,20 +52,32 @@ st.write(
 
 question = st.text_area("Enter you question:")
 if st.button("Ask"):
-    query=f"SET @query='{question}';  SET @options = JSON_Object('vector_store',JSON_ARRAY('vectordb.demo_embedding'));call sys.ML_RAG(@query,@output,@options);SELECT JSON_UNQUOTE(JSON_EXTRACT(@output, '$.text'));"
-    # query=f"set @query='test';set @options = JSON_OBJECT('vector_store', JSON_ARRAY('vectordb.demo_embeddings'));call sys.ML_RAG(@query,@output,@option);select JSON_PRETTY(@output);"
-    answer = sql_rest_query(query, resp_format='json')
+    q=f"call sys.heatwave_chat('{question}')"
+    qtest="call sys.heatwave_chat('what is heatwave auto ml')"
+    # query=f"SET @query ='{question}';  SET @options = JSON_Object('vector_store',JSON_ARRAY('vectordb.demo_embedding'));call sys.ML_RAG(@query,@output,@options);SELECT JSON_UNQUOTE(JSON_EXTRACT(@output, '$.text')) as output;"
+    query=f"set @query='test';set @options = JSON_OBJECT('vector_store', JSON_ARRAY('vectordb.demo_embeddings'));call sys.ML_RAG(@query,@output,@option);"
+    # answer = sql_rest_query(query, resp_format='json')
     # answer = sql_rest_query('show tables in vectordb')
-    # q1="set @options = JSON_OBJECT('vector_store', JSON_ARRAY('vectordb.demo_embeddings'));"
+    # answer = sql_rest_query(query,resp_format='table')
+    q1="set @options = JSON_OBJECT('vector_store', JSON_ARRAY('vectordb.demo_embeddings'));"
     # a1=a1=requests.post(endpoint, data=q1, headers=headers, auth=auth)
-    # q2=f"set @query='{question}'"
+    q2=f"set @query='{question}'"
     # a2=requests.post(endpoint, data=q2, headers=headers, auth=auth)
-    # q3="call sys.ML_RAG(@query,@output,@options);"
+    q3="call sys.ML_RAG(@query,@output,@options);"
     # a3=requests.post(endpoint, data=q3, headers=headers, auth=auth)
-    # q4="SELECT JSON_UNQUOTE(JSON_EXTRACT(@output, '$.text'));"
+    q4="SELECT JSON_UNQUOTE(JSON_EXTRACT(@output, '$.text')) as output;"
     # a4=requests.post(endpoint, data=q4, headers=headers, auth=auth)
     # answer=requests.post(endpoint, data=query, headers=headers, auth=auth)
-    if answer is not None:
-        st.write(answer[0])
+    # df1 = conn.query(f'{query}', ttl=600)
+    test_query="select JSON_PRETTY(@output); as output"
+    with conn.session as session:
+        session.execute(text(query))
+    df1 = conn.query(f'{q4}', ttl=600) 
+    if df1 is not None:
+        for row in df1.itertuples():
+            st.write(f"{row.output}:")
+        # st.write(df1)    
+        # st.write(answer)
+        # st.write(answer[6])
     else:
         st.write("DB connection error")
